2ccc9a318012be344742dde9757f784a9e5f3851
Sothea <89758627+sotheaq@users.noreply.github.com>
2025-07-28 18:27:11 +0700

    Create product.js

---
+++ product.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+  {
+    id: "001",
+    image: "images/pgytech-cardreader.webp",
+    name: "PGY Tech Card Reader",
+    description: "Fast and Secure",
+  },
+];
+
+export { products };
